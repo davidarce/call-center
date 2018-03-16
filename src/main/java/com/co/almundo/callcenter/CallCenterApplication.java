552252ef61ff28a009c1375517355f0dbb813fb2
Oscar Arce <oscar.arce@ceiba.com.co>
2018-03-16 13:08:12 -0500

    agregando estructura del proyecto

---
+++ src/main/java/com/co/almundo/callcenter/CallCenterApplication.java
@@ -0,0 +1,12 @@
+package com.co.almundo.callcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallCenterApplication.class, args);
+	}
+}
